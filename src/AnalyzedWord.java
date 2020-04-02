9ec2c8fe7db4c29142ae2f6ca5b9d150e79c9dc7
Adrian <hadrian7775@gmail.com>
2020-04-01 23:19:32 -0400

    Commit before replacing hard code with HTMLtoPlaintext method.

--- src/AnalyzedWord.java
+++ src/AnalyzedWord.java
@@ -1,15 +1,32 @@
-
+/**
+ * 
+ * An object used to store a word, and how often it occurs, comparable by amount of occurrences.
+ *
+ */
 public class AnalyzedWord implements Comparable<AnalyzedWord> {
+	
 	
 	private String word;
 	private int occurences;
 	
+	
+	/**
+	 * Creates new AnalyzedWord Object containing a string passed by the user, and starting with 1 occurrence.
+	 * @param word Word to be stored for tracking occurrences.
+	 * 
+	 * 
+	 */
 	public AnalyzedWord(String word) {
 		
 		this.word = word;
 		this.occurences = 1;
 	}
 	
+	/**
+	 * 
+	 * Adds one occurrence of the word to the occurrence count.
+	 * 
+	 */
 	
 	public void addOccurence() {
 		
@@ -17,22 +34,45 @@ public class AnalyzedWord implements Comparable<AnalyzedWord> {
 	}
 	
 	
+	/**
+	 * Gets tracked word.
+	 * @return Word being tracked.
+	 */
+	
 	public String getWord() {
 		return word;
 	}
 
+	/**
+	 * Sets tracked word.
+	 * @param word Word being tracked.
+	 */
+	
 	public void setWord(String word) {
 		this.word = word;
 	}
 
+	/**
+	 * Gets current number of times this word has appeared.
+	 * @return How many times this word has appeared in text.
+	 */
+	
 	public int getOccurences() {
 		return occurences;
 	}
 
+	/**
+	 * Sets current number of times this word has appeared.
+	 * @param occurences How many times this word has appeared in text.
+	 */
 	public void setOccurences(int occurences) {
 		this.occurences = occurences;
 	}
 
+	/**
+	 * Makes object comparable based on number of occurrences.
+	 * 
+	 */
 
 	@Override
 	public int compareTo(AnalyzedWord b) {
--- src/HTMLtoPlaintext.java
+++ src/HTMLtoPlaintext.java
@@ -3,8 +3,25 @@ import java.net.URL;
 
 import org.jsoup.Jsoup;
 
+/**
+ * 
+ * 
+ * 
+ * 
+ *  This class converts HTML text to plain text, with no punctuation or HTML tags. 
+ *
+ */
+
+
 public class HTMLtoPlaintext {
 
+	/**
+	 * This method uses the JSOUP API and a regular expression to remove HTML coding, then punctuation.
+	 * @see org.jsoup.Jsoup
+	 * @param url The URL containing the HTML code that is to be parsed into plain text.
+	 * @return A String containing plain text, with no punctuation or HTML tags
+	 */
+	
 	public static String parseHtml(URL url) {
 		
 		
